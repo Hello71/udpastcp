aac5ecd47b3008d770671a024c5f078fd74640a2
Alex Xu (Hello71) <alex_y_xu@yahoo.ca>
2016-07-02 08:58:13 -0400

    Add bidirectional testing.

--- test.sh
+++ test.sh
@@ -1,18 +1,26 @@
 #!/bin/sh
 # this script tests basic udpastcp functionality.
-if (
-pids=
-trap 'kill $pids' EXIT
-./udpastcp client localhost 36563 localhost 64109 &
-pids="$!"
-./udpastcp server localhost 64109 localhost 41465 &
-pids+=" $!"
-socat udp6-listen:41465 - &
-pids+=" $!"
-( sleep 1; echo AAAAAAAA | socat - 'udp-connect:[::1]:36563' ) &
-pids+=" $!"
-sleep 2
-) 2>/dev/null | grep AAAAAAAA >/dev/null; then
+
+test1() {
+    (
+        pids=
+        trap 'kill $pids' EXIT
+        ./udpastcp client localhost 36563 localhost 64109 &
+        pids="$!"
+        ./udpastcp server localhost 64109 localhost 41465 &
+        pids+=" $!"
+        ( ( sleep 0.2; echo BBBBBBBB; ) | socat udp6-listen:41465 - ) &
+        pids+=" $!"
+        ( ( sleep 0.1; echo AAAAAAAA; ) | socat - 'udp-connect:[::1]:36563' ) &
+        pids+=" $!"
+        sleep 0.3
+    )
+}
+
+nl='
+'
+
+if [ "$( test1 )" = "AAAAAAAA${nl}BBBBBBBB" ]; then
     echo "Test succeeded."
 else
     echo "Test failed."
