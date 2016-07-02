61e60c05097d7def8643a5a66bb6bc26229494a7
Alex Xu <alex_y_xu@yahoo.ca>
2016-07-02 19:12:23 -0400

    Make timing of test less sensitive.

--- test.sh
+++ test.sh
@@ -8,12 +8,12 @@ test1() {
         ./udpastcp client localhost 36563 localhost 64109 &
         pids="$!"
         ./udpastcp server localhost 64109 localhost 41465 &
-        pids+=" $!"
-        ( ( sleep 0.2; echo BBBBBBBB; ) | socat udp6-listen:41465 - ) &
-        pids+=" $!"
-        ( ( sleep 0.1; echo AAAAAAAA; ) | socat - 'udp-connect:[::1]:36563' ) &
-        pids+=" $!"
-        sleep 0.3
+        pids="$pids $!"
+        ( ( sleep 0.4; echo BBBBBBBB; ) | socat udp6-listen:41465 - ) &
+        pids="$pids $!"
+        ( ( sleep 0.2; echo AAAAAAAA; ) | socat - 'udp-connect:[::1]:36563' ) &
+        pids="$pids $!"
+        sleep 0.5
     )
 }
 
